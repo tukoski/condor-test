7ef23b1531b25ab03f120cf84f3bdf71c8bf436c
WalterM <walterm@jetsemani.com>
2019-05-21 14:33:47 -0500

    Remove next files

--- next.config.js
+++ next.config.js
@@ -1,21 +1,27 @@
-const withSass = require("@zeit/next-sass")
-const withCss = require("@zeit/next-css")
+const withSass = require("@zeit/next-sass");
+const withCss = require("@zeit/next-css");
 
 module.exports = withCss(
   withSass({
+    exportPathMap: function() {
+      return {
+        "/": { page: "/" }
+      };
+    },
+    assetPrefix: process.env.NODE_ENV === "production" ? "/condor-test/" : "",
     distDir: "../build",
     webpack(config) {
-      config.resolve.modules.push("src")
+      config.resolve.modules.push("src");
       config.module.rules.push({
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|otf)$/,
         use: {
           loader: "url-loader",
           options: {
-            limit: 100000,
-          },
-        },
-      })
-      return config
-    },
+            limit: 100000
+          }
+        }
+      });
+      return config;
+    }
   })
-)
+);
